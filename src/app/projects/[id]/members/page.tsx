94eb1576129d52870826d99d33777474c7884b9f
JakubGaleta <jgaleta@seznam.cz>
2023-11-24 00:03:24 +0100

    added staring html and code for /project/members

--- src/app/projects/[id]/members/page.tsx
+++ src/app/projects/[id]/members/page.tsx
@@ -1,8 +1,59 @@
+"use client"
+
+import { useState } from "react"
+
+type MemberInfo = {
+    name: string,
+    surname: string,
+    position: string,
+    skills: string, //TODO: diferent type
+}
+// TODO: tags for position "skills"
 
 export default function Members() {
+    const [ members, setMember] = useState<MemberInfo>();
+    
+    async function fetchMembers() {
+
+    }
+    
     return (
-        <main>
+        <main className="px-14 py-14">
             <h1>Members</h1>
+            <TableMembers/>
         </main>
+    )
+}
+
+function SearchMembers() {
+
+}
+
+function FilterMembers() {
+
+}
+
+function TableMembers() {
+    return (
+        <table className=" w-max">
+            <thead className="w-max">
+                <tr>
+                    <th>name</th>
+                    <th>position</th>
+                    <th>skills</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    )
+}
+
+function Member({ memberInfo } : { memberInfo : MemberInfo }) {
+    return (
+        <tr>
+            
+        </tr>
     )
 }
