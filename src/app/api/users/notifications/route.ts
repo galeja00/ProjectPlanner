313b26524f8bdf98db2224bfd9361d7b2608c05f
JakubGaleta <jgaleta@seznam.cz>
2024-02-13 18:07:34 +0100

    more work on notifications for user

--- src/app/api/users/notifications/route.ts
+++ src/app/api/users/notifications/route.ts
@@ -1,19 +1,20 @@
-import { ProjectInvite } from "@prisma/client";
+import { Project, ProjectInvite } from "@prisma/client";
 import { prisma } from "@/db";
 import { DateTime } from "next-auth/providers/kakao";
-import Project from "@/app/projects/[id]/page";
 import { authorize, getUserId } from "../../static";
 
 type Notification = {
     id : string,
     projectId : string,
+    name : string,
+    icon : string | null,
     type : NotificationTypes,
     creatAt : Date,
     displayd : boolean
 }
 
 enum NotificationTypes {
-    ProjectInvite = "Project invite"
+    ProjectInvite = "ProjectInvite"
 }
 
 export async function GET(req : Request, { params } : { params : { id : string }}) {
@@ -35,13 +36,22 @@ export async function GET(req : Request, { params } : { params : { id : string }
         })
 
         for (const projectInvite of projectInvites) {
+            const project : Project | null = await prisma.project.findFirst({
+                where: {
+                    id: projectInvite.projectId
+                },
+            })
+            if (project == null) {
+                throw new Error();
+            }
             const notif : Notification = { 
                 id:  projectInvite.id, 
                 projectId: projectInvite.projectId,
                 type: NotificationTypes.ProjectInvite, 
                 creatAt: projectInvite.createAt, 
                 displayd: projectInvite.displayed, 
-
+                name: project.name,
+                icon: project.icon
             }
             notifications.push(notif);
         }
--- src/app/notifications/notifications.tsx
+++ src/app/notifications/notifications.tsx
@@ -7,11 +7,18 @@ import { DateTime } from "next-auth/providers/kakao"
 type Notification = {
     id : string,
     projectId : string,
+    name : string,
+    icon : string | null,
     type : string,
     creatAt : Date,
     displayd : boolean
 }
 
+enum NotifictionsText {
+    ProjectInvite = "You have been invited to project: "
+}
+
+//TODO: fliters ...
 export default function NotifiactionsList() {
     const [notifs, setNotifs] = useState<Notification[]>([]); 
     
@@ -47,16 +54,36 @@ export default function NotifiactionsList() {
     )
 }
 
+// TODO: accept/decline and design
 function NotificationsItem({notif} : {notif : Notification}) {
+    const currentDate: Date = new Date();
+    var time : number = currentDate.getTime() - notif.creatAt.getTime() / (1000 * 60 * 60); // in Hours
+    const day : number = 24
+    if (time > day) { //If longer the one day then convert o days
+        time = time / day;
+    }
+
+    var text : string;
+    switch (notif.type) {
+        case "ProjectInvite":
+            text = NotifictionsText.ProjectInvite;
+            break;
+        default:
+            text = NotifictionsText.ProjectInvite
+    }
+
     return (
         <li className='bg-neural-900'>
             <Image src={''} alt={''} width={30} height={30} className="bg-neutral-50 rounded h-fit block mt-auto mb-auto"></Image>
             <div>
-                <h5>name</h5>
-                <div>7h</div>
+                <h3>{notif.name}</h3>
+                <p>{text}</p>
+                <time>{time}</time>
             </div>
-            
-            <p>text</p>
+            <div>
+                <button className="btn-primary">Accept</button>
+                <button className="">Decline</button>
+            </div>
         </li>
     )
 } 
