b5af329ded6ce9b049fa288e07d58fc2b1498ae6
JakubGaleta <jgaleta@seznam.cz>
2024-02-29 00:01:45 +0100

    start UI tamplate for backlog

--- src/app/projects/[id]/[...boards]/backlog.tsx
+++ src/app/projects/[id]/[...boards]/backlog.tsx
@@ -1,9 +1,57 @@
+import { FilterButton, SearchInput } from "../components/filter-tables";
+import { Head } from "../components/other";
 
 export default function Backlog() {
     return (
-        <div className="py-14 px-14 relative w-full">
-            <h1>backlog</h1>
+        <div className="w-2/3 mx-auto">
+            <Head text="Backlog"/>
+            <section className='flex gap-4 mb-4 w-fit h-fit items-end'>
+                <SearchInput/>
+                <FilterButton/>
+            </section>
+            <ListOfGroups/>
         </div>
         
+    )
+}
+
+function GroupCreate() {
+    return (
+        <button className="flex gap-2 items-center mb-2 text-neutral-400">
+            <img src="/plus.svg" className="w-8 h-8 bg-neutral-950 rounded"/>
+            <div>Create new group</div>
+        </button>
+    )
+}
+
+function ListOfGroups() {
+    return (
+        <section className="w-full">
+            <GroupCreate/>
+            <ul className="space-y-4 w-full">
+                <GroupList/>
+                <GroupList/>
+                <GroupList/>
+            </ul>
+        </section>
+    )
+}
+
+function GroupList() {
+    return (
+        <li className="bg-neutral-950 w-full rounded p-2 space-y-2">
+            <h2>Name Of Group</h2>
+            <ul className="space-y-2">
+                <GroupTask/>
+            </ul>
+        </li> 
+    )
+}
+
+function GroupTask() {
+    return (
+        <li className="bg-neutral-900 w-full p-2 rounded">
+            <h3>tohle je dokonala taska</h3>
+        </li>
     )
 }
