144c2d7e966975785f3bace09a47b879d3b087f0
Jakub Galeta <99719012+galeja00@users.noreply.github.com>
2024-06-17 10:26:05 +0200

    Deshboard deleted, now main site is Projects when you user is loged in app.

--- src/app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Dashboard() {
-    return (
-        <main className="flex min-h-screen flex-col items-center p-24 ">
-            <h1>Dashbard</h1>
-        </main>
-    )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ export default async function Home() {
   const session = await getServerSession(options);
 
   if (session) {
-    redirect("/dashboard");
+    redirect("/projects");
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24 ">
       <h1 className='mb-10'>Start planning your great projects and teams with Project planner</h1>
-      <Link href="auth/signup" className='btn-primary'>Start for free</Link>
+      <Link href="auth/signup" className='btn-primary'>Sign Up</Link>
     </main>
   )
 }
