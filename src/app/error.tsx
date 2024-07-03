7f265cc4ddfb6c800d3fecad0d4449364e720fef
Jakub Galeta <99719012+galeja00@users.noreply.github.com>
2024-07-03 15:23:30 +0200

    Upgreat of error message

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,9 +3,9 @@
 
 export default function Error({ error, reset } : { error : Error & { digest?: string }, reset : () => void}) {
     return (
-        <div>
+        <div className="bg-neutral-950 rounnded p-4 m-auto flex justify-center flex-col gap-4">
           <h2>An error ocurred: {error.message}</h2>
-          <button onClick={() => reset()}>Try again</button>
+          <button className="btn-primary" onClick={() => reset()}>Try again</button>
         </div>
       )
 }
